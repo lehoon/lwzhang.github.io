35c00ac180bb15bb7d7730daa1ce5c7d14d3e58e
lwzhang <656615764@qq.com>
2016-01-17 17:16:25 +0800

    Site updated: 2016-01-17 17:15:50

--- js/main.js
+++ js/main.js
@@ -102,9 +102,9 @@ require([], function (){
 		
 	}
 
-	if($(".instagram").length != 0){
-		require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(ins){
-			ins.init();
+	if($(".instagram").length) {
+		require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
+			obj.init();
 		});
 	}
 	
